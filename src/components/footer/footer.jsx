6f43734991c24d3469d6c1f3bdebbcd19dca3923
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2026-02-16 22:22:56 +0200

    import

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.scss";
+
+function footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="container"></div>
+      </footer>
+    </>
+  );
+}
+
+export default footer;
